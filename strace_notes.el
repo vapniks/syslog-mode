15fc03155b3786e5e6a2cbbd83a5254c1a5c9b40
Joe Bloggs <vapniks@yahoo.com>
2021-08-17 16:27:48 +0100

    update strace_notes.el

--- strace_notes.el
+++ strace_notes.el
@@ -8,21 +8,25 @@
 (setq syslog-notes (list
 		    ;; signal descriptions are found in the signal(7) manpage
 		    '("ABRT\\|ALRM\\|BUS\\|CHLD\\|CLD\\|CONT\\|EMT\\|FPE\\|HUP\\|ILL\\|INFO\\|INT\\|IO\\|IOT\\|KILL\\|LOST\\|PIPE\\|POLL\\|PROF\\|PWR\\|QUIT\\|SEGV\\|STKFLT\\|STOP\\|SYS\\|TERM\\|TRAP\\|TSTP\\|TTIN\\|TTOU\\|UNUSED\\|URG\\|USR1\\|USR2\\|VTALRM\\|WINCH\\|XCPU\\|XFSZ"
-		      "rt_sigprocmask" syslog-show-note-from-manpages (lambda (word) (concat "SIG" word)) "signal(7)")
+		      "\\<rt_sigprocmask\\((\\| resumed\\)" syslog-show-note-from-manpages
+		      (lambda (word) (concat "SIG" word)) "signal(7)")
 		    ;; ioctl has several different manpages
-		    '(nil "^\\S-+ \\(ioctl\\)(" syslog-show-note-from-manpages
+		    '(nil "\\<ioctl\\((\\| resumed\\)" syslog-show-note-from-manpages
 			  word ("ioctl_console" "ioctl_fat" "ioctl_ficlonerange" "ioctl_fideduperange"
 				"ioctl_getfsmap" "ioctl_iflags" "ioctl_list" "ioctl_ns" "ioctl_tty"
 				"ioctl_userfaultfd"))
 		    ;; termios struct flags are found in the termios(3) manpage
 		    '("-?\\(ignbrk\\|brkint\\|ignpar\\|parmrk\\|inpck\\|istrip\\|inlcr\\|igncr\\|icrnl\\|iuclc\\|ixon\\|ixany\\|ixoff\\|imaxbel\\|iutf8\\|opost\\|olcuc\\|onlcr\\|ocrnl\\|onocr\\|onlret\\|ofill\\|ofdel\\|nldly\\|crdly\\|tabdly\\|bsdly\\|vtdly\\|ffdly\\|cbaud\\|cbaudex\\|csize\\|cstopb\\|cread\\|parenb\\|parodd\\|hupcl\\|clocal\\|loblk\\|cibaud\\|cmspar\\|crtscts\\|isig\\|icanon\\|xcase\\|echo\\|echoe\\|echok\\|echonl\\|echoctl\\|echoprt\\|echoke\\|defecho\\|flusho\\|noflsh\\|tostop\\|pendin\\|iexten\\|vdiscard\\|vdsusp\\|veof\\|veol\\|veol2\\|verase\\|vintr\\|vkill\\|vlnext\\|vmin\\|vquit\\|vreprint\\|vstart\\|vstatus\\|vstop\\|vsusp\\|vswtch\\|vtime\\|vwerase\\)"
-		      "ioctl(" syslog-show-note-from-manpages
+		      "\\<ioctl\\((\\| resumed\\)" syslog-show-note-from-manpages
 		      (lambda (word) (upcase (replace-regexp-in-string "^-" "" word)))
 		      "termios(3)")
 		    ;; rt_sigaction uses different indentations for different types of words 
-		    '(nil "^\\S-+ \\(rt_sigaction\\)(" syslog-show-note-from-manpages word line t 11)
-		    '("sa_[a-z]+" "^\\S-+ \\(rt_sigaction\\)("
-		      syslog-show-note-from-manpages word line t 7 Man-underline)
+		    '(nil "\\<rt_sigaction\\((\\| resumed\\)"
+			  syslog-show-note-from-manpages word "rt_sigaction" t 11)
+		    '("sa_[a-z]+" "\\<rt_sigaction\\((\\| resumed\\)"
+		      syslog-show-note-from-manpages word "rt_sigaction" t 7 Man-underline)
 		    ;; by default search manpage of function at start of line
 		    '(nil "^\\S-+ \\([^(]+\\)(" syslog-show-note-from-manpages word line)
+		    ;; it could also be resumed from a previous line
+		    '(nil "^\\S-+ <... \\(\\S-+\\) resumed" syslog-show-note-from-manpages word line)
 		    ))
