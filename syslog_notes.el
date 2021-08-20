5bb177b68271478f8390b6d9a66c358c1e08c064
Joe Bloggs <vapniks@yahoo.com>
2021-08-20 03:19:57 +0100

    Add syslog_notes.el

---
+++ syslog_notes.el
@@ -0,0 +1,9 @@
+;; This file contains a definition of `syslog-notes' used by the `syslog-show-notes' function.
+;; See the `syslog-notes' documentation for info about the correct format
+;; After editing save & kill this buffer, and then in the syslog-mode buffer do: M-x syslog-load-notes
+;; To always use this file add an entry to the `syslog-notes-files' user option.
+;; See also `syslog-show-note-from-manpages' `syslog-show-note-from-file-or-buffer',
+;; `syslog-show-note-from-info-node' and `syslog-show-note-from-apropos'
+(setq-local syslog-notes (list
+			  '(nil nil syslog-show-note-from-apropos word t)
+			  ))
